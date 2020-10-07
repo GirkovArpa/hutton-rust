24901fff3aa6378cc6888507cbccd7fa91d6de4b
GirkovArpa <GirkovArpa@protonmail.com>
2020-10-07 00:50:12 -0500

    :construction: Backup while I go play League of Legends.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,22 @@ fn main() {
     let abc = String::from("abcdefghijklmnopqrstuvwxyz");
     let output = create_key(&input, &abc);
     println!("{}", output);
+
+    let input = String::from("helloworld");
+    let password = String::from("foo");
+    let key = String::from("bar");
+    let output = encrypt(&input, &password, &key);
+    println!("{}", output);
+}
+
+fn encrypt(input: &str, password: &str, key: &str) -> String {
+    let abc = String::from("abcdefghijklmnopqrstuvwxyz");
+    let mut alphabet_key = create_key(&key, &abc);
+    let mut output = String::new();
+    for (i, c) in input.iter().enumerate() {
+
+    }
+    abc
 }
 
 fn rotate_string(input: &str) -> String {
