ccd0208d568a70911f2639de4ad517bbdb3dd7a7
GirkovArpa <GirkovArpa@protonmail.com>
2020-10-07 00:24:52 -0500

    :sparkles: Add rotate_string function.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 fn main() {
-    let input: &str = "foo";
-    let output = swap_chars(input, 0, 2);
+    let input = String::from("foo");
+    let output = swap_chars(&input, 0, 2);
     println!("{}", output);
+
+    let input = String::from("foo");
+    let output = rotate_string(&input);
+    println!("{}", output);
+}
+
+fn rotate_string(input: &str) -> String {
+    let first = input.chars().nth(0).unwrap();
+    let rest = remove_first(&input);
+    rest + &first.to_string()
+}
+
+fn remove_first(input: &str) -> String {
+    let mut output = input.chars();
+    output.next();
+    String::from(output.as_str())
 }
 
 fn swap_chars(input: &str, i: usize, j: usize) -> String {
     let a = input.chars().nth(i).unwrap();
     let b = input.chars().nth(j).unwrap();
-    input.chars()
+    input
+        .chars()
         .enumerate()
-        .map(|(idx, c)| 
-            match idx {
-                idx if idx == i => b,
-                idx if idx == j => a,
-                _ => c
-            }
-        )
+        .map(|(idx, c)| match idx {
+            idx if idx == i => b,
+            idx if idx == j => a,
+            _ => c,
+        })
         .collect()
 }
